a2637e524caa10bc6e2fee28d945fa73e21b4639
Duran Sakalli <45287656+duransakalli@users.noreply.github.com>
2019-12-12 22:29:36 -0800

    Add files via upload

--- IdentifySmithNumbers.java
+++ IdentifySmithNumbers.java
@@ -1,65 +1,42 @@
 package Solutions;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
 
--public class IdentifySmithNumbers {
+public class IdentifySmithNumbers {
 
-    // Complete the solve function below.
-    static int solve(int n) {
-    	
-    	int i = 2;
-    	int sum = 0;
-    	int sum2 = 0;
-    	
-    	while(i == n)
-    	{
-    		if(n % i == 0)
-    		{
-    			sum +=i;
-    			n /= i;
-    			i = 1;
-    		}	
-    			
-    		i++;
-    	}
-    	
-    	
-    	int num = n;
-        
-        while (num > 0) {
-            sum2 = sum2 + num % 10;
-            num = num / 10;
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int s=0;
+        int b=a;
+        for(int x=2;x<=a;x++)
+        {
+            while(a%x==0)
+            {
+                a=a/x;
+                int q=x;
+                while(q>0)
+                {
+                    s=s+q%10;
+                    q=q/10;
+                }
+            }
+        }        
+        int u=0;
+        while(b>0)
+        {
+            u=u+b%10;
+            b=b/10;
         }
         
-        
-        if(sum == sum2)
+        if(u==s)
         {
-        	return 1;
-        } else
-        {
-        	return 0;
+            System.out.println("1");
         }
-
-    }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int result = solve(n);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
+        else
+        {
+            System.out.println("0");
+        }
+        sc.close();
     }
 }
